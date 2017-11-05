0c86617ec0b8c0d8840d693f7d7964ee1c3eb948
Ary Baldioceda <abaldioceda@gmail.com>
2017-11-05 21:20:50 +0000

    Done.

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,15 @@
+class FiguresController < ApplicationController
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+
+
+  end
+
+end
